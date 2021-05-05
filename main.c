b73cb97b44dde0ccad7927e0883c3910a90f0daf
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-05-05 02:58:15 +0200

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int fil[5] = {0, 1, 2, 3, 4};
 sem_t mutex;
 sem_t fork[5];
 
-void eat(int filnum){\
+void test(int filnum){\
 
     if( state[filnum] == GLOD && state[((filnum + 4)% 5)] != JEDZENIE && state[((filnum + 1)% 5)] != JEDZENIE){
 
@@ -30,7 +30,22 @@ void eat(int filnum){\
 
 }
 
+void put_fork(int filnum){
 
+    sem_wait(&mutex);
+
+    state[filnum] = MYSLENIE;
+
+    printf("Filozof %d odlozyl widelec %d i widelec %d", filnum + 1, ((filnum + 4)% 5))+1, filnum + 1);
+
+    printf("Filozof %d mysli", filnum + 1);
+
+    test(((filnum + 4)% 5)));
+    test(((filnum + 1)% 5))v);
+
+    sem_post(&mutex);
+
+}
 
 void take_fork(int filnum){
 
@@ -40,7 +55,7 @@ void take_fork(int filnum){
 
     printf("Filozof %d jest glodny", filnum + 1);
 
-    eat(filnum);
+    test(filnum);
     sem_post(&mutex);
     sem_wait(&fork[filnum]);
 

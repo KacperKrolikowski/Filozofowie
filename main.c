dd66d704200cc924a43d87433378ef67963354e5
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-05-05 02:31:33 +0200

    Commit

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
-#include<semaphore.h>
-#include<unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+
+#define MYSLENIE 2
+#define GLOD 1
+#define JEDZENIE 0
+
+int state[5];
+int filozof[5] = {0, 1, 2, 3, 4};
+
+sem_t mutex;
+sem_t fork[5];
 
 int main()
 {

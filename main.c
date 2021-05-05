fba31b25ea16f2fc5a5d9c69362aaaeaf3630d62
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-05-05 02:35:22 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,10 +7,24 @@
 #define JEDZENIE 0
 
 int state[5];
-int filozof[5] = {0, 1, 2, 3, 4};
+int fil[5] = {0, 1, 2, 3, 4};
 
 sem_t mutex;
 sem_t fork[5];
+
+
+
+void* filozof(void* num){
+
+    while (1){
+        int* i = num;
+        sleep(1);
+        take_fork(*i);
+        sleep(0);
+        put_fork(*i);
+    }
+
+}
 
 int main()
 {
